7ba24bd4926093a59a9448ee870950aaac90dd74
Aiman Abdel-Samad <aiman.samad@zemoga.com>
2016-03-01 18:48:18 -0500

    second commit :P

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -10,5 +10,29 @@ angular.module('myContacts.contacts', ['ngRoute', 'firebase'])
 }])
 
 .controller('ContactsCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-	console.log($scope);
+	console.log('ContactsCtrl init');
+	var ref = new Firebase('https://mycontacts-angular-udemy.firebaseio.com/');
+
+	$scope.contacts = $firebaseArray(ref);
+	console.log('ref=', $scope);
+
+	$scope.showAddForm = function (b) {
+		$scope.addFormShow = b;
+	}
+
+	$scope.addFormSubmit = function () {
+		console.log('Adding contact...');
+		var name = ($scope.name) ? $scope.name : null,
+			email = ($scope.email) ? $scope.email : null,
+			company = ($scope.company) ? $scope.company : null,
+			mobile_phone = ($scope.mobile_phone) ? $scope.mobile_phone : null,
+			home_phone = ($scope.home_phone) ? $scope.home_phone : null,
+			work_phone = ($scope.work_phone) ? $scope.work_phone : null,
+			street_address = ($scope.street_address) ? $scope.street_address : null,
+			city = ($scope.city) ? $scope.city : null,
+			state = ($scope.state) ? $scope.state : null,
+			zipcode = ($scope.zipcode) ? $scope.zipcode : null;
+
+		//$scope.contacts.$add({name: name, email:email, })
+	}
 }]);
